ce93b8727bc37d47e68159812827efa2b8fceb8e
Makana-Grey <makanagreyx@gmail.com>
2022-11-26 00:45:44 +0300

    mirror generator completion, refactoring of picture gen and new pattern gen

--- test/index.js
+++ test/index.js
@@ -5,10 +5,11 @@ import {
 } from "../lib/colorGenerator.js";
 import { generateMirror } from "../lib/mirrorGenerator.js";
 import { generatePicture } from "../lib/pictureGenerator.js";
+import { generatePattern } from "../lib/patternGenerator.js";
 
-const cellSize = 50;
+const cellSize = 40;
 
-const pattern = `
+const template = `
 . . . . . . .!
 . . . . . . .!
 . . . @ . . .!
@@ -34,6 +35,8 @@ const options = {
     y: cellSize,
   },
 };
+
+const pattern = generatePattern(template);
 
 const halfPicture = generatePicture(pattern, options);
 const pictire = generateMirror(
